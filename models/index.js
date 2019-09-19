5b5d97ac0113a6d828741a98482a3ffbbcc4a087
Michael Sabzevari <michaelsabzevari@gmail.com>
2019-09-19 13:43:01 -0400

    heroku deployment

--- models/index.js
+++ models/index.js
@@ -3,27 +3,27 @@ const UserModel = require('./user')
 const BioModel = require('./bio')
 const bcrypt = require('bcrypt')
 
-const db = new Sequelize(process.env.DATABASE_URL, {
-  // database: "bio_user",
+const db = new Sequelize((process.env.DATABASE_URL || 'postgres://localhost:5432/bio_user'), {
+  database: "bio_user",
   dialect: 'postgres',
   define: {
     underscored: true,
     returning: true
   }
 })
-// if (process.env.NODE_ENV === 'production') {
-//   // If the node environment is production, connect to a remote PSQL database
-//   const db = new Sequelize(process.env.DATABASE_URL , {
-//     dialect: 'postgres'
-//   });
-// }
-// else {
-//   // Else connect to a local instance of PSQL running on your machine
-//   const db = new Sequelize({
-//     database: '', // Name of your local database
-//     dialect: 'postgres'
-//   });
-// }
+if (process.env.NODE_ENV === 'production') {
+  // If the node environment is production, connect to a remote PSQL database
+  const db = new Sequelize(process.env.DATABASE_URL , {
+    dialect: 'postgres'
+  });
+}
+else {
+  // Else connect to a local instance of PSQL running on your machine
+  const db = new Sequelize({
+    database: '', // Name of your local database
+    dialect: 'postgres'
+  });
+}
 
 const User = UserModel(db, Sequelize);
 
