8bcb8134253596987deeb854d62c3495e81a417a
Michael Sabzevari <michaelsabzevari@gmail.com>
2020-07-02 12:36:09 -0400

    changes to back end

--- client/src/services/apiService.js
+++ client/src/services/apiService.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
-// const BASE_URL = process.env.REACT_APP_HEROKU_URL || 'http://localhost:8001'
-const BASE_URL = 'http://localhost:8001'
+const BASE_URL = process.env.REACT_APP_HEROKU_URL || 'http://localhost:8001'
+// const BASE_URL = 'http://localhost:8001'
 const JWT_TOKEN = localStorage.getItem('token')
 
 const apiClient = axios.create({
