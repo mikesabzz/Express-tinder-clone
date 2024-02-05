52359b7636d138594c6529467576500462f37e84
mikesabzz <michaelsabzevari@gmail.com>
2024-02-04 22:07:23 -0500

    found the bcrypt error

--- auth/auth.js
+++ auth/auth.js
@@ -2,7 +2,7 @@ const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
 const { User } = require('../models/index.js')
 const jwt = require('jsonwebtoken')
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs')
 const JWTStrategy = require('passport-jwt').Strategy
 const ExtractJWT = require('passport-jwt').ExtractJwt
 require('dotenv').config()
